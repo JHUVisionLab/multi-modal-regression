e754954f7fa73ee0e1700b95a72d5c2cfb65a265
Siddharth Mahendran <siddharthm28@gmail.com>
2018-10-06 21:33:50 -0400

    Added LogEuclidean and updated Probabilistic BD models

--- learnLogEuclideanModel.py
+++ learnLogEuclideanModel.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Geodesic Bin and Delta model for the axis-angle representation
+Log-Euclidean Bin and Delta model for the axis-angle representation
 """
 
 import torch
@@ -8,15 +8,15 @@ from torch import nn, optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from dataGenerators import Pascal3dAll, my_collate
-from binDeltaGenerators import XPBDGenerator
-from axisAngle import get_error2, geodesic_loss
+from dataGenerators import TestImages, my_collate
+from binDeltaGenerators import GBDGenerator
+from axisAngle import get_error2, get_R, get_y
 from binDeltaModels import OneBinDeltaModel, OneDeltaPerBinModel
-from binDeltaLosses import SimpleRelaXedLoss, RelaXedLoss
-from helperFunctions import classes, get_gamma
+from helperFunctions import classes
 
 import numpy as np
 import scipy.io as spio
+import math
 import gc
 import os
 import time
@@ -25,13 +25,13 @@ import pickle
 import argparse
 from tensorboardX import SummaryWriter
 
-parser = argparse.ArgumentParser(description='RelaXed Bin & Delta Model')
+parser = argparse.ArgumentParser(description='Log-Euclidean Bin & Delta Model')
 parser.add_argument('--gpu_id', type=str, default='0')
 parser.add_argument('--render_path', type=str, default='data/renderforcnn/')
 parser.add_argument('--augmented_path', type=str, default='data/augmented2/')
-parser.add_argument('--pascal3d_path', type=str, default='data/original')
+parser.add_argument('--pascal3d_path', type=str, default='data/flipped_new/test')
 parser.add_argument('--save_str', type=str)
-parser.add_argument('--dict_size', type=int, default=100)
+parser.add_argument('--dict_size', type=int, default=200)
 parser.add_argument('--num_workers', type=int, default=4)
 parser.add_argument('--feature_network', type=str, default='resnet')
 parser.add_argument('--N0', type=int, default=2048)
@@ -42,7 +42,6 @@ parser.add_argument('--init_lr', type=float, default=1e-4)
 parser.add_argument('--num_epochs', type=int, default=3)
 parser.add_argument('--max_iterations', type=float, default=np.inf)
 parser.add_argument('--multires', type=bool, default=False)
-parser.add_argument('--alpha', type=float, default=1.0)
 args = parser.parse_args()
 print(args)
 # assign GPU
@@ -57,26 +56,25 @@ log_dir = os.path.join('logs', args.save_str)
 # kmeans data
 kmeans_file = 'data/kmeans_dictionary_axis_angle_' + str(args.dict_size) + '.pkl'
 kmeans = pickle.load(open(kmeans_file, 'rb'))
-kmeans_dict = kmeans.cluster_centers_
 num_clusters = kmeans.n_clusters
-gamma = get_gamma(kmeans_dict)
-print('Gamma: ', gamma)
+rotations_dict = np.stack([get_R(kmeans.cluster_centers_[i]) for i in range(kmeans.n_clusters)])
 
 # relevant variables
 ndim = 3
 num_classes = len(classes)
 
-criterion1 = SimpleRelaXedLoss(args.alpha)
-criterion2 = RelaXedLoss(args.alpha, kmeans_file, geodesic_loss().cuda())
+# loss
+mse_loss = nn.MSELoss().cuda()
+ce_loss = nn.CrossEntropyLoss().cuda()
 
 # DATA
 # datasets
-real_data = XPBDGenerator(args.augmented_path, 'real', kmeans_file, gamma)
-render_data = XPBDGenerator(args.render_path, 'render', kmeans_file, gamma)
-test_data = Pascal3dAll(args.pascal3d_path, 'test')
+real_data = GBDGenerator(args.augmented_path, 'real', kmeans_file)
+render_data = GBDGenerator(args.render_path, 'render', kmeans_file)
+test_data = TestImages(args.pascal3d_path)
 # setup data loaders
 real_loader = DataLoader(real_data, batch_size=args.num_workers, shuffle=True, num_workers=args.num_workers, pin_memory=True, collate_fn=my_collate)
-render_loader = DataLoader(render_data, batch_size=4, shuffle=True, num_workers=4, pin_memory=True, collate_fn=my_collate)
+render_loader = DataLoader(render_data, batch_size=args.num_workers, shuffle=True, num_workers=args.num_workers, pin_memory=True, collate_fn=my_collate)
 test_loader = DataLoader(test_data, batch_size=32, collate_fn=my_collate)
 print('Real: {0} \t Render: {1} \t Test: {2}'.format(len(real_loader), len(render_loader), len(test_loader)))
 
@@ -99,81 +97,62 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
 writer = SummaryWriter(log_dir)
 count = 0
 val_loss = []
+s = 0
+
+
+def get_residuals(ydata):
+	ydata_res = np.zeros((ydata.shape[0], len(rotations_dict), 3))
+	for i in range(ydata.shape[0]):
+		for j in range(len(rotations_dict)):
+			ydata_res[i, j, :] = get_y(np.dot(rotations_dict[j].T, get_R(ydata[i])))
+	return ydata_res
 
 
 # OPTIMIZATION functions
-def training_init():
-	global count, val_loss
-	model.train()
-	bar = progressbar.ProgressBar(max_value=max_iterations)
-	for i, (sample_real, sample_render) in enumerate(zip(real_loader, render_loader)):
-		# forward steps
-		xdata_real = Variable(sample_real['xdata'].cuda())
-		label_real = Variable(sample_real['label'].cuda())
-		ydata_real = [Variable(sample_real['ydata_bin'].cuda()), Variable(sample_real['ydata_res'].cuda())]
-		output_real = model(xdata_real, label_real)
-		loss_real = criterion1(output_real, ydata_real)
-		xdata_render = Variable(sample_render['xdata'].cuda())
-		label_render = Variable(sample_render['label'].cuda())
-		ydata_render = [Variable(sample_render['ydata_bin'].cuda()), Variable(sample_render['ydata_res'].cuda())]
-		output_render = model(xdata_render, label_render)
-		loss_render = criterion1(output_render, ydata_render)
-		loss = loss_real + loss_render
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-		# store
-		writer.add_scalar('train_loss', loss.item(), count)
-		if i % 1000 == 0:
-			ytest, yhat_test, test_labels = testing()
-			spio.savemat(results_file, {'ytest': ytest, 'yhat_test': yhat_test, 'test_labels': test_labels})
-			tmp_val_loss = get_error2(ytest, yhat_test, test_labels, num_classes)
-			writer.add_scalar('val_loss', tmp_val_loss, count)
-			val_loss.append(tmp_val_loss)
-		count += 1
-		# cleanup
-		del xdata_real, xdata_render, label_real, label_render, ydata_real, ydata_render
-		del output_real, output_render, loss_real, loss_render, sample_real, sample_render, loss
-		bar.update(i)
-		# stop
-		if i == max_iterations:
-			break
-	render_loader.dataset.shuffle_images()
-	real_loader.dataset.shuffle_images()
-
-
 def training():
-	global count, val_loss
+	global count, val_loss, s
 	model.train()
 	bar = progressbar.ProgressBar(max_value=max_iterations)
 	for i, (sample_real, sample_render) in enumerate(zip(real_loader, render_loader)):
 		# forward steps
 		xdata_real = Variable(sample_real['xdata'].cuda())
 		label_real = Variable(sample_real['label'].cuda())
-		ydata_real = [Variable(sample_real['ydata_bin'].cuda()), Variable(sample_real['ydata'].cuda())]
 		output_real = model(xdata_real, label_real)
-		loss_real = criterion2(output_real, ydata_real)
 		xdata_render = Variable(sample_render['xdata'].cuda())
 		label_render = Variable(sample_render['label'].cuda())
-		ydata_render = [Variable(sample_render['ydata_bin'].cuda()), Variable(sample_render['ydata'].cuda())]
 		output_render = model(xdata_render, label_render)
-		loss_render = criterion2(output_render, ydata_render)
-		loss = loss_real + loss_render
+		# loss
+		ydata_bin = torch.cat((Variable(sample_real['ydata_bin'].cuda()), Variable(sample_render['ydata_bin'].cuda())))
+		output_bin = torch.cat((output_real[0], output_render[0]))
+		Lc = ce_loss(output_bin, ydata_bin)
+		labels = torch.argmax(output_bin, dim=1)
+		labels = torch.zeros(labels.size(0), num_clusters).scatter_(1, labels.unsqueeze(1).data.cpu(), 1.0)
+		labels = Variable(labels.unsqueeze(2).cuda())
+		ydata_numpy = np.concatenate((sample_real['ydata'].data.cpu().numpy(), sample_render['ydata'].data.cpu().numpy()))
+		ydata_res = Variable(torch.from_numpy(get_residuals(ydata_numpy)).cuda())
+		ydata_res = torch.squeeze(torch.bmm(ydata_res.permute(0, 2, 1), labels), 2)
+		output_res = torch.cat((output_real[1], output_render[1]))
+		Lr = mse_loss(output_res, ydata_res)
+		loss = Lc + 0.5*math.exp(-2*s)*Lr + s
+		# parameter updates
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
+		s = 0.5*math.log(Lr)
 		# store
+		count += 1
 		writer.add_scalar('train_loss', loss.item(), count)
+		writer.add_scalar('alpha', 0.5*math.exp(-2*s), count)
 		if i % 1000 == 0:
 			ytest, yhat_test, test_labels = testing()
 			spio.savemat(results_file, {'ytest': ytest, 'yhat_test': yhat_test, 'test_labels': test_labels})
 			tmp_val_loss = get_error2(ytest, yhat_test, test_labels, num_classes)
 			writer.add_scalar('val_loss', tmp_val_loss, count)
 			val_loss.append(tmp_val_loss)
-		count += 1
 		# cleanup
-		del xdata_real, xdata_render, label_real, label_render, ydata_real, ydata_render
-		del output_real, output_render, loss_real, loss_render, sample_real, sample_render, loss
+		del xdata_real, xdata_render, label_real, label_render
+		del output_bin, output_res, ydata_bin, ydata_res, labels
+		del output_real, output_render, sample_real, sample_render, loss
 		bar.update(i)
 		# stop
 		if i == max_iterations:
@@ -193,7 +172,8 @@ def testing():
 		output = model(xdata, label)
 		ypred_bin = np.argmax(output[0].data.cpu().numpy(), axis=1)
 		ypred_res = output[1].data.cpu().numpy()
-		ypred.append(kmeans_dict[ypred_bin, :] + ypred_res)
+		y = [get_y(np.dot(rotations_dict[ypred_bin[j]], get_R(ypred_res[j]))) for j in range(ypred_bin.shape[0])]
+		ypred.append(y)
 		ytrue.append(sample['ydata'].numpy())
 		labels.append(sample['label'].numpy())
 		del xdata, label, output, sample
@@ -209,11 +189,7 @@ def save_checkpoint(filename):
 	torch.save(model.state_dict(), filename)
 
 
-# initialization
-training_init()
-ytest, yhat_test, test_labels = testing()
-print('\nMedErr: {0}'.format(get_error2(ytest, yhat_test, test_labels, num_classes)))
-
+# train
 for epoch in range(args.num_epochs):
 	tic = time.time()
 	scheduler.step()
--- learnProbabilisticBDModel.py
+++ learnProbabilisticBDModel.py
@@ -1,147 +1,164 @@
 # -*- coding: utf-8 -*-
 """
-Function that learns feature model + 3layer pose models x 12 object categories
-in an end-to-end manner by minimizing the mean squared error for axis-angle representation
+Geodesic Bin and Delta model for the axis-angle representation
 """
 
 import torch
 from torch import nn, optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
-from dataGenerators import MultibinImages, Pascal3dAll, my_collate
-from featureModels import resnet_model
-from axisAngle import get_error, geodesic_loss
-from binDeltaModels import bin_3layer, res_3layer, loss_m3
+from dataGenerators import Pascal3dAll, my_collate
+from binDeltaGenerators import XPBDGenerator
+from axisAngle import get_error2, geodesic_loss
+from binDeltaModels import OneBinDeltaModel, ProbabilisticOneDeltaPerBinModel
+from binDeltaLosses import RelaXedProbabilisticLoss, RelaXedProbabilisticMultiresLoss
 from helperFunctions import classes
 
 import numpy as np
 import scipy.io as spio
+import math
 import gc
 import os
 import time
 import progressbar
-import sys
 import pickle
+import argparse
+from tensorboardX import SummaryWriter
 
-if len(sys.argv) > 1:
-	os.environ['CUDA_VISIBLE_DEVICES'] = sys.argv[1]
-
-# relevant paths
-render_path = 'data/renderforcnn/'
-augmented_path = 'data/augmented2/'
-pascal3d_path = 'data/original'
+parser = argparse.ArgumentParser(description='RelaXed Probabilistic Bin & Delta Model')
+parser.add_argument('--gpu_id', type=str, default='0')
+parser.add_argument('--render_path', type=str, default='data/renderforcnn/')
+parser.add_argument('--augmented_path', type=str, default='data/augmented2/')
+parser.add_argument('--pascal3d_path', type=str, default='data/original')
+parser.add_argument('--save_str', type=str)
+parser.add_argument('--dict_size', type=int, default=100)
+parser.add_argument('--num_workers', type=int, default=4)
+parser.add_argument('--feature_network', type=str, default='resnet')
+parser.add_argument('--N0', type=int, default=2048)
+parser.add_argument('--N1', type=int, default=1000)
+parser.add_argument('--N2', type=int, default=500)
+parser.add_argument('--N3', type=int, default=100)
+parser.add_argument('--init_lr', type=float, default=1e-4)
+parser.add_argument('--num_epochs', type=int, default=3)
+parser.add_argument('--max_iterations', type=float, default=np.inf)
+parser.add_argument('--multires', type=bool, default=False)
+parser.add_argument('--alpha', type=float, default=1.0)
+args = parser.parse_args()
+print(args)
+# assign GPU
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
 
 # save stuff here
-save_str = 'resnet50_3layer_m3_1'
-results_file = os.path.join('results', save_str)
-model_file = os.path.join('models', save_str + '.tar')
-plots_file = os.path.join('plots', save_str)
+results_file = os.path.join('results', args.save_str)
+model_file = os.path.join('models', args.save_str + '.tar')
+plots_file = os.path.join('plots', args.save_str)
+log_dir = os.path.join('logs', args.save_str)
 
-# kmeans data
-kmeans_file = 'data/kmeans_dictionary_axis_angle_100.pkl'
-kmeans = pickle.load(open(kmeans_file, 'rb'))
-kmeans_dict = kmeans.cluster_centers_
-num_clusters = kmeans.n_clusters
+# gmm data
+gmm_file = 'data/gmm_dictionary_axis_angle_' + str(args.dict_size) + '.pkl'
+gmm = pickle.load(open(gmm_file, 'rb'))
+gmm_dict = gmm.means_
+num_clusters = gmm.n_components
+cluster_centers = Variable(torch.from_numpy(gmm_dict).float()).cuda()
 
 # relevant variables
 ndim = 3
-num_workers = 4
-N0 = 2048
-N1 = 1000
-N2 = 500
 num_classes = len(classes)
-init_lr = 0.0001
-num_epochs = 3
 
-problem_type = 'm3'
-criterion = loss_m3(1.0, kmeans_file, geodesic_loss(reduce=False).cuda())
+# loss
+kl_div = nn.KLDivLoss().cuda()
+gve_loss = geodesic_loss(reduce=False).cuda()
 
 # DATA
 # datasets
-real_data = MultibinImages(augmented_path, 'real', problem_type, kmeans_file)
-render_data = MultibinImages(render_path, 'render', problem_type, kmeans_file)
-test_data = Pascal3dAll(pascal3d_path, 'test')
+real_data = XPBDGenerator(args.augmented_path, 'real', gmm_file)
+render_data = XPBDGenerator(args.render_path, 'render', gmm_file)
+test_data = Pascal3dAll(args.pascal3d_path, 'test')
 # setup data loaders
-real_loader = DataLoader(real_data, batch_size=4, shuffle=True, num_workers=num_workers, pin_memory=True, collate_fn=my_collate)
-render_loader = DataLoader(render_data, batch_size=4, shuffle=True, num_workers=num_workers, pin_memory=True, collate_fn=my_collate)
+real_loader = DataLoader(real_data, batch_size=args.num_workers, shuffle=True, num_workers=args.num_workers, pin_memory=True, collate_fn=my_collate)
+render_loader = DataLoader(render_data, batch_size=args.num_workers, shuffle=True, num_workers=args.num_workers, pin_memory=True, collate_fn=my_collate)
 test_loader = DataLoader(test_data, batch_size=32, collate_fn=my_collate)
 print('Real: {0} \t Render: {1} \t Test: {2}'.format(len(real_loader), len(render_loader), len(test_loader)))
 
-
-# MODEL
-# my model for pose estimation: feature model + 1layer pose model x 12
-class my_model(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.num_classes = num_classes
-		self.feature_model = resnet_model('resnet50', 'layer4').cuda()
-		self.bin_models = nn.ModuleList([bin_3layer(N0, N1, N2, num_clusters) for i in range(self.num_classes)]).cuda()
-		self.res_models = nn.ModuleList([res_3layer(N0, N1, N2, ndim) for i in range(self.num_classes)]).cuda()
-
-	def forward(self, x, label):
-		x = self.feature_model(x)
-		y1 = torch.stack([self.bin_models[i](x) for i in range(self.num_classes)]).permute(1, 2, 0)
-		y2 = torch.stack([self.res_models[i](x) for i in range(self.num_classes)]).permute(1, 2, 0)
-		label = torch.zeros(label.size(0), self.num_classes).scatter_(1, label.data.cpu(), 1.0)
-		label = Variable(label.unsqueeze(2).cuda())
-		y1 = torch.squeeze(torch.bmm(y1, label), 2)
-		y2 = torch.squeeze(torch.bmm(y2, label), 2)
-		del x, label
-		return [y1, y2]
-
+if np.isinf(args.max_iterations):
+	max_iterations = min(len(real_loader), len(render_loader))
+else:
+	max_iterations = args.max_iterations
 
 # my_model
-model = my_model()
+if not args.multires:
+	model = OneBinDeltaModel(args.feature_network, num_classes, num_clusters, args.N0, args.N1, args.N2, ndim)
+else:
+	model = ProbabilisticOneDeltaPerBinModel(args.feature_network, num_classes, num_clusters, args.N0, args.N1, args.N2, args.N3, ndim)
+
 # print(model)
 # loss and optimizer
-optimizer = optim.Adam(model.parameters(), lr=init_lr)
+optimizer = optim.Adam(model.parameters(), lr=args.init_lr)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-train_loss = []
-train_loss_sum = 0.0
-train_samples = 0
+# store stuff
+writer = SummaryWriter(log_dir)
+count = 0
+val_loss = []
+s = 0
 
 
 # OPTIMIZATION functions
-def training(save_loss=False):
-	global train_loss_sum
-	global train_samples
+def training():
+	global count, val_loss, s
 	model.train()
-	print(criterion)
-	bar = progressbar.ProgressBar(max_value=len(render_loader))
+	bar = progressbar.ProgressBar(max_value=max_iterations)
 	for i, (sample_real, sample_render) in enumerate(zip(real_loader, render_loader)):
 		# forward steps
 		xdata_real = Variable(sample_real['xdata'].cuda())
 		label_real = Variable(sample_real['label'].cuda())
-		ydata_real = [Variable(sample_real['ydata_bin'].cuda()), Variable(sample_real['ydata'].cuda())]
 		output_real = model(xdata_real, label_real)
-		loss_real = criterion(output_real, ydata_real)
 		xdata_render = Variable(sample_render['xdata'].cuda())
 		label_render = Variable(sample_render['label'].cuda())
-		ydata_render = [Variable(sample_render['ydata_bin'].cuda()), Variable(sample_render['ydata'].cuda())]
 		output_render = model(xdata_render, label_render)
-		loss_render = criterion(output_render, ydata_render)
-		loss = loss_real + loss_render
+		# loss
+		ydata_bin = torch.cat((Variable(sample_real['ydata_bin'].cuda()), Variable(sample_render['ydata_bin'].cuda())))
+		output_bin = torch.cat((output_real[0], output_render[0]))
+		Lc = kl_div(F.log_softmax(output_bin, dim=1), ydata_bin)
+		ydata = torch.cat((Variable(sample_real['ydata'].cuda()), Variable(sample_render['ydata'].cuda())))
+		output_res = torch.cat((output_real[1], output_render[1]))
+		if not args.multires:
+			Lr = torch.stack([gve_loss(ydata, torch.add(output_res, 1.0, cluster_centers.index_select(0, Variable(j*torch.ones(1).long().cuda())))) for j in range(num_clusters)])
+		else:
+			y = cluster_centers + output_res
+			Lr = torch.stack([gve_loss(ydata, torch.squeeze(y.index_select(1, Variable(j * torch.ones(1).long().cuda())))) for j in range(num_clusters)])
+		Lr = torch.mean(torch.sum(torch.mul(F.softmax(output_bin, dim=1), torch.t(Lr)), dim=1))
+		loss = Lc + math.exp(-s)*Lr + s
+		# updates
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
+		s = math.log(Lr)
 		# store
-		bar.update(i)
-		train_loss_sum += (loss_real.data[0] * xdata_real.size(0) + loss_render.data[0] * xdata_render.size(0))
-		train_samples += (xdata_real.size(0) + xdata_render.size(0))
-		if i % 1000 == 0 and save_loss:
-			train_loss.append(train_loss_sum / train_samples)
+		count += 1
+		writer.add_scalar('train_loss', loss.item(), count)
+		writer.add_scalar('alpha', math.exp(-s), count)
+		if i % 1000 == 0:
+			ytest, yhat_test, test_labels = testing()
+			spio.savemat(results_file, {'ytest': ytest, 'yhat_test': yhat_test, 'test_labels': test_labels})
+			tmp_val_loss = get_error2(ytest, yhat_test, test_labels, num_classes)
+			writer.add_scalar('val_loss', tmp_val_loss, count)
+			val_loss.append(tmp_val_loss)
 		# cleanup
-		del xdata_real, xdata_render, label_real, label_render, ydata_real, ydata_render
-		del output_real, output_render, loss_real, loss_render, sample_real, sample_render, loss
-		gc.collect()
+		del xdata_real, xdata_render, label_real, label_render
+		del ydata_bin, output_bin, ydata, output_res
+		del output_real, output_render, sample_real, sample_render, loss
+		bar.update(i)
+		# stop
+		if i == max_iterations:
+			break
 	render_loader.dataset.shuffle_images()
 	real_loader.dataset.shuffle_images()
 
 
 def testing():
 	model.eval()
-	bar = progressbar.ProgressBar(max_value=len(test_loader))
 	ypred = []
 	ytrue = []
 	labels = []
@@ -149,12 +166,24 @@ def testing():
 		xdata = Variable(sample['xdata'].cuda())
 		label = Variable(sample['label'].cuda())
 		output = model(xdata, label)
-		ypred_bin = np.argmax(output[0].data.cpu().numpy(), axis=1)
-		ypred_res = output[1].data.cpu().numpy()
-		ypred.append(kmeans_dict[ypred_bin, :] + ypred_res)
+		if not args.multires:
+			# ypred_bin = F.softmax(output[0], dim=1).data.cpu().numpy()
+			# ypred_res = output[1].data.cpu().numpy()
+			# tmp_ypred = np.dot(ypred_bin, gmm_dict) + ypred_res
+			ypred_bin = np.argmax(output[0].data.cpu().numpy(), axis=1)
+			ypred_res = output[1].data.cpu().numpy()
+			tmp_ypred = gmm_dict[ypred_bin, :] + ypred_res
+		else:
+			# ypred_bin = F.softmax(output[0], dim=1).data.cpu().numpy()
+			# ypred_res = output[1].data.cpu().numpy()
+			# tmp_ypred = np.stack([np.dot(gmm_dict.T + ypred_res[i].T, ypred_bin[i]) for i in range(ypred_bin.shape[0])])
+			ypred_bin = np.argmax(output[0].data.cpu().numpy(), axis=1)
+			ypred_res = output[1].data.cpu().numpy()
+			tmp_ypred = np.stack(
+				[gmm_dict[ypred_bin[i]] + ypred_res[i, ypred_bin[i], :] for i in range(ypred_bin.shape[0])])
+		ypred.append(tmp_ypred)
 		ytrue.append(sample['ydata'].numpy())
 		labels.append(sample['label'].numpy())
-		bar.update(i)
 		del xdata, label, output, sample
 		gc.collect()
 	ypred = np.concatenate(ypred)
@@ -168,23 +197,30 @@ def save_checkpoint(filename):
 	torch.save(model.state_dict(), filename)
 
 
-# run optimization
-for epoch in range(num_epochs):
+# evaluate the model
+ytest, yhat_test, test_labels = testing()
+print('\nMedErr: {0}'.format(get_error2(ytest, yhat_test, test_labels, num_classes)))
+
+for epoch in range(args.num_epochs):
 	tic = time.time()
 	scheduler.step()
 	# training step
-	training(True)
+	training()
 	# save model at end of epoch
 	save_checkpoint(model_file)
+	# validation
+	ytest, yhat_test, test_labels = testing()
+	print('\nMedErr: {0}'.format(get_error2(ytest, yhat_test, test_labels, num_classes)))
 	# time and output
 	toc = time.time() - tic
 	print('Epoch: {0} done in time {1}s'.format(epoch, toc))
 	# cleanup
 	gc.collect()
-# save plots
-spio.savemat(plots_file, {'train_loss': train_loss})
+writer.close()
+val_loss = np.stack(val_loss)
+spio.savemat(plots_file, {'val_loss': val_loss})
 
 # evaluate the model
 ytest, yhat_test, test_labels = testing()
-get_error(ytest, yhat_test)
+print('\nMedErr: {0}'.format(get_error2(ytest, yhat_test, test_labels, num_classes)))
 spio.savemat(results_file, {'ytest': ytest, 'yhat_test': yhat_test, 'test_labels': test_labels})
